5d6faceaa9414e1938877c29de5f986936d101ea
Kendric <kendricwashington1402@gmail.com>
2024-08-28 14:41:48 -0500

    HW1 Commit

---
+++ Hw1/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 300);
+
+  
+  background(0, 128, 0);
+  fill('purple');
+  ellipse(185,100,450,150)
+
+  fill('white');
+  ellipse(90,100,100,100);
+  ellipse(280,100,100,100);
+
+  noFill();
+  ellipse(180,240,180,1,);
+
+
+  describe('A white circle with black outline in the middle of a gray canvas.');
+}
